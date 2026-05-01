4a77903e0614c0c80bd1d2e80aac757bf1451ff3
aniketbarai <aniketbarai677@gmail.com>
2026-05-01 07:28:10 +0530

    Added Searchbar and updated the navbar.

--- note/frontend/notes-app/src/pages/Home/Home.jsx
+++ note/frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar/Navbar'
 const Home = () => {
   return (
-    <div className='bg-amber-500'>
-      Home
-    </div>
+    <>
+    <Navbar />
+
+    <div></div>
+    </>
   )
 }
 
--- note/frontend/notes-app/src/pages/Login/Login.jsx
+++ note/frontend/notes-app/src/pages/Login/Login.jsx
@@ -42,8 +42,6 @@ const Login = () => {
 
           {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
 
-
-
         <button type="submit" className="btn-primary" >Login</button>
         <p className="text-sm text-center mt-4">
           Not registered yet?{" "}
